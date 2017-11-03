f2ecd69ba2ef2b0d1c08909b8e0b51ff701270b7
dianlujitao <dianlujitao@lineageos.org>
2017-11-03 21:34:14 +0800

    op3: Squashed revert camera dirty hack
    Revert "op3: Add shim to make front camera not take 4 MP pics in bright light"
    
    This reverts commit c46f9bd903962ca1a7511580f19252bbdc91f222.
    
    Change-Id: Ie2cfce843a7b8e5b7921c0aeeb58ca783f914114
    
    Revert "op3: Add shim to fix front facing camera crash in bright light"
    
    This reverts commit ec5325bf033fdf79a3bcf977ae7d1b6057283809.
    
    Change-Id: I43619800b3b16160777b47602bc808ffd0668aca
    
    Revert "op3: Use shim to set camera package name in the HAL to OnePlus Camera"
    
    This reverts commit 7eaff84a2346f7e82d794c305f3afdfaac734fca.
    
    Change-Id: Ic52e924229edd3432006579c318eb2ea3dbd0db4
    
    Revert "op3: Add camera shim to always disable QuadraCfa mode"
    
    This reverts commit 4dab60e0c4d4adfb30eac1380a91affea7ec13db.
    
    Change-Id: I487c8d2444cef22982baaec3e8960f3599993ae3
    
    Revert "op3: Set camera package name to OnePlus Camera"
    
    This reverts commit 079e43fb8782f604bf9d2688d5e8cd4f53a743eb.
    
    Change-Id: I97ae93891ff598590405e0232eab5f99b6c7b03d
    
    Revert "op3: Add specific camera parameter library"
    
    This reverts commit 46b7928bccd575deed3dfcc3c2f8ec65bf89d560.
    
    Change-Id: I34107212a78f7ef575710a1be4963655f38cfd22
    
    Revert "op3: Remove camera shim library"
    
    This reverts commit d4986087e4f4e832731270580d54368c5c99d4a9.
    
    Change-Id: I7ba779a5ad9862e415d3cec0e03fa0a7b9d82f7c
    
    Revert "op3: Automatically sed camera HAL symbol changes needed for shim"
    
    This reverts commit 4fe0ea5ad648a9c10c9ca305f9f3a8f60dbdb181.
    
    Change-Id: Ifd668e3bd29c9c8d8e98bbefe5213cefd554e332

--- device.mk
+++ device.mk
@@ -159,7 +159,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libcamera_shim \
+    Snap \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
 
