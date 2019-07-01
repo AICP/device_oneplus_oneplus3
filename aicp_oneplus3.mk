f74d12deb76a7968d9138e914bfa6eecbad42abf
Darragh McGee <darraghmcgee20@gmail.com>
2019-07-01 13:40:17 +0200

    oneplus3: Update maintainer
    Change-Id: I1b49e7acb4af03414ff108e1fc9b5fb2bda398a1

--- aicp_oneplus3.mk
+++ aicp_oneplus3.mk
@@ -33,3 +33,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1905251415:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Darragh McGee (DarkExistence)"
